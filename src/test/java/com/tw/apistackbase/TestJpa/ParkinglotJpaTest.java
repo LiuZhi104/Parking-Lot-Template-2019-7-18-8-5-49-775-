37bc346474318c3524ff30366b36f81152dac18f
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-18 22:23:07 +0800

    [Jeffrey][test:parkingJpaTest should find parkinglots information when has parkinglot page and pagesize   ]

--- src/test/java/com/tw/apistackbase/TestJpa/ParkinglotJpaTest.java
+++ src/test/java/com/tw/apistackbase/TestJpa/ParkinglotJpaTest.java
@@ -47,6 +47,7 @@ public class ParkinglotJpaTest {
         Parkinglot parkinglot3=new Parkinglot(3,"parkthree",10,"胡同2");
         parkinglotRepository.save(parkinglot3);
         Parkinglot parkinglot4=new Parkinglot(4,"parkfour",10,"胡同3");
+        parkinglotRepository.save(parkinglot4);
         //when
         parkinglotRepository.deleteById(1);
         List<Parkinglot> parkinglots=parkinglotRepository.findAll();
@@ -63,6 +64,20 @@ public class ParkinglotJpaTest {
             parkinglotRepository.saveAndFlush(parkinglot);
 
         });
+    }
+    @Test
+    public  void should_find_parkinglots_information_when_has_parkinglot_page_and_pagesize(){
+        //given
+        Parkinglot parkinglot1=new Parkinglot(1,"parkone",10,"胡同");
+        parkinglotRepository.save(parkinglot1);
+        Parkinglot parkinglot2=new Parkinglot(2,"parktwo",10,"胡同1");
+        parkinglotRepository.save(parkinglot2);
+        Parkinglot parkinglot3=new Parkinglot(3,"parkthree",10,"胡同2");
+        parkinglotRepository.save(parkinglot3);
+        //when
+        List<Parkinglot> parkinglots=parkinglotRepository.findAllByPageAndPageSize(1,15);
+        //then
+        Assertions.assertEquals(3,parkinglots.size());
     }
 
 
